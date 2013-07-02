8de483ced7ec727efee153c71d3addc0c8264a2b
ncornette <nicolas.cornette@gmail.com>
2013-07-02 23:14:47 +0200

    ZoomableView enabled

--- demo/src/main/java/com/zoomableview/HelloAndroidActivity.java
+++ demo/src/main/java/com/zoomableview/HelloAndroidActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.zoomableview.DepMapViewTouchable.TouchMapListener;
+
 public class HelloAndroidActivity extends Activity {
 
     /**
@@ -16,7 +18,35 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DepMapViewScalable zoomable = (DepMapViewScalable) findViewById(R.id.zoomable);
+        final DepMapViewScalable zoomable = (DepMapViewScalable) findViewById(R.id.zoomable);
+        zoomable.setMap(R.drawable.chicken_cartoon_500);
+        zoomable.setTouchMapListener(new TouchMapListener() {
+
+            @Override
+            public void onTouchScale(float arg0, float arg1, float arg2) {
+            }
+
+            @Override
+            public void onTouchDep(float arg0, float arg1) {
+            }
+
+            @Override
+            public void onSingleTapConfirmed() {
+            }
+
+            @Override
+            public void onSingleTapCancelled() {
+            }
+
+            @Override
+            public void onDoubleTap(float arg0, float arg1) {
+                if (zoomable.isZoomed()) {
+                    zoomable.zoomOut();
+                } else {
+                    zoomable.zoomOnScreen(arg0, arg1);
+                }
+            }
+        });
     }
 
     @Override
