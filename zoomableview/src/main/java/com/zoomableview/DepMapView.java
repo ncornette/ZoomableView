f314cd458da1883509043c9c1f83ef238db2137d
ncornette <nicolas.cornette@gmail.com>
2013-07-02 22:20:35 +0200

    code reformat, update config

--- zoomableview/src/main/java/com/zoomableview/DepMapView.java
+++ zoomableview/src/main/java/com/zoomableview/DepMapView.java
@@ -12,75 +12,73 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * @author nic
- * 
- * A View that draws a Map and can show departments on top of it, the department
- * can blink and the related region can be displayed. Department capitals can
- * be displayed too. 
- *
+ * @author nic A View that draws a Map and can show departments on top of it, the department can
+ *         blink and the related region can be displayed. Department capitals can be displayed too.
  */
 public class DepMapView extends View {
 
-	private Bitmap map;
-	private Paint mapPaint;
-	protected RectF rectMapOrigin = new RectF();
-	protected RectF rectView = new RectF();
-	protected Matrix matrixOrigin = new Matrix();
-	protected Matrix matrix = new Matrix();
-	
-	public DepMapView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		mapPaint = new Paint();
-		mapPaint.setFilterBitmap(true);
-		
-		map = null;
-	}
-	
-	/**
-	 * Sets the image resource to be displayed for the map 
-	 * @param resId id of a drawable
-	 */
-	public void setMap(int resId) {
-		this.map = BitmapFactory.decodeResource(getResources(), resId);
-		requestLayout();
-	}
-	
-	/**
-	 * Sets the image to be displayed for the map 
-	 * @param bmp Bitmap to show
-	 */
-	public void setMap(Bitmap bmp) {
-		this.map = bmp;
-		requestLayout();
-	}
-	
-	@Override
-	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-		super.onLayout(changed, left, top, right, bottom);
-		rectMapOrigin.set(0f, 0f, map.getWidth(), map.getHeight());
-		rectView.set(0f, 0f, getWidth(), getHeight());
-		matrixOrigin.setRectToRect(rectMapOrigin, rectView, ScaleToFit.CENTER);
-		if (matrix.isIdentity()) {
-			matrix.set(matrixOrigin);
-		}
-	}
+    private Bitmap map;
+    private Paint mapPaint;
+    protected RectF rectMapOrigin = new RectF();
+    protected RectF rectView = new RectF();
+    protected Matrix matrixOrigin = new Matrix();
+    protected Matrix matrix = new Matrix();
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
+    public DepMapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mapPaint = new Paint();
+        mapPaint.setFilterBitmap(true);
 
-		canvas.concat(matrix);
-		
-		// Draw full map
-		canvas.drawBitmap(map, 0, 0, mapPaint);
-		
-//		// Debug clip Bounds
-//		Paint clipPaint = new Paint();
-//		clipPaint.setColor(Color.RED);
-//		clipPaint.setStyle(Style.STROKE);
-//		clipPaint.setStrokeWidth(5);
-//		canvas.drawRect(canvas.getClipBounds(), clipPaint);
+        map = null;
+    }
 
-	}
+    /**
+     * Sets the image resource to be displayed for the map
+     * 
+     * @param resId id of a drawable
+     */
+    public void setMap(int resId) {
+        this.map = BitmapFactory.decodeResource(getResources(), resId);
+        requestLayout();
+    }
+
+    /**
+     * Sets the image to be displayed for the map
+     * 
+     * @param bmp Bitmap to show
+     */
+    public void setMap(Bitmap bmp) {
+        this.map = bmp;
+        requestLayout();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        rectMapOrigin.set(0f, 0f, map.getWidth(), map.getHeight());
+        rectView.set(0f, 0f, getWidth(), getHeight());
+        matrixOrigin.setRectToRect(rectMapOrigin, rectView, ScaleToFit.CENTER);
+        if (matrix.isIdentity()) {
+            matrix.set(matrixOrigin);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.concat(matrix);
+
+        // Draw full map
+        canvas.drawBitmap(map, 0, 0, mapPaint);
+
+        // // Debug clip Bounds
+        // Paint clipPaint = new Paint();
+        // clipPaint.setColor(Color.RED);
+        // clipPaint.setStyle(Style.STROKE);
+        // clipPaint.setStrokeWidth(5);
+        // canvas.drawRect(canvas.getClipBounds(), clipPaint);
+
+    }
 
 }
--- zoomableview/src/main/java/com/zoomableview/DepMapViewScalable.java
+++ zoomableview/src/main/java/com/zoomableview/DepMapViewScalable.java
@@ -12,88 +12,90 @@ import android.view.animation.BounceInterpolator;
 @TargetApi(value = 8)
 public class DepMapViewScalable extends DepMapViewTouchable implements OnScaleGestureListener {
 
-	private android.view.ScaleGestureDetector scaleDetector;
-	private float[] matrixValues = new float[9];
-	private float[] matrixOriginValues = new float[9];
-	protected Boolean scaling = null;
-	Matrix savedMatrix = new Matrix();
+    private android.view.ScaleGestureDetector scaleDetector;
+    private float[] matrixValues = new float[9];
+    private float[] matrixOriginValues = new float[9];
+    protected Boolean scaling = null;
+    Matrix savedMatrix = new Matrix();
 
-	public DepMapViewScalable(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		scaleDetector = new android.view.ScaleGestureDetector(this.getContext(), this);
-	}
+    public DepMapViewScalable(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        scaleDetector = new android.view.ScaleGestureDetector(this.getContext(), this);
+    }
 
-	@Override
-	protected void onLayout(boolean changed, int left, int top, int right,
-			int bottom) {
-		super.onLayout(changed, left, top, right, bottom);
-		matrixOrigin.getValues(matrixOriginValues);
-	}
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        matrixOrigin.getValues(matrixOriginValues);
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		scaleDetector.onTouchEvent(event);
-		return super.onTouchEvent(event);
-	}
-	
-	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-			float distanceY) {
-		
-		if (Boolean.FALSE.equals(scaling)) {
-			scaling=null;
-			return false;
-		}
-		if (scaling != null) return false;
-		return super.onScroll(e1, e2, distanceX, distanceY);
-	}
-	
-	@Override
-	public boolean onScale(android.view.ScaleGestureDetector detector) {
-		if (!zooming()) {
-           savedMatrix.set(matrix);
-           matrix.postScale(detector.getScaleFactor(), detector.getScaleFactor(), detector.getFocusX(), detector.getFocusY());
-           matrix.getValues(matrixValues);
-           if (matrixValues[Matrix.MSCALE_X]<matrixOriginValues[Matrix.MSCALE_X]/1.5f ||
-	           matrixValues[Matrix.MSCALE_X]>matrixOriginValues[Matrix.MSCALE_X]*5f) {
-	           matrix.set(savedMatrix);
-           } else {
-        	   invalidate();
-           }
-		}
-		return true;
-	}
-	@Override
-	public boolean onScaleBegin(android.view.ScaleGestureDetector detector) {
-		mapListener.onSingleTapCancelled();
-		mapListener.onTouchScale(detector.getScaleFactor(), 
-				detector.getFocusX(), detector.getFocusY());
-		scaling = true;
-		zoomed = true;
-		return true;
-	}
-	@Override
-	public void onScaleEnd(android.view.ScaleGestureDetector detector) {
-		Log.i(getClass().getSimpleName(), "Scale End");
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        scaleDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
-		matrix.getValues(matrixValues);
-		scaling = false;
-		
-		if (matrixValues[Matrix.MSCALE_X]<matrixOriginValues[Matrix.MSCALE_X]) {
-			mapScaleAnim = new MapScaleAnim(matrix, matrixOrigin, 200);
-			zoomed = false;
-		} else if (matrixValues[Matrix.MSCALE_X]>matrixOriginValues[Matrix.MSCALE_X]*3f) {
-			mapScaleAnim = new MapScaleAnim(matrix, getWidth()/2, getHeight()/2, getWidth()/2, getHeight()/2, 
-					matrixOriginValues[Matrix.MSCALE_X]/matrixValues[Matrix.MSCALE_X]*3f, 200);
-		} else {
-			return;
-		}
-		
-		mapScaleAnim.initialize(0, 0, getWidth(), getHeight());
-		mapScaleAnim.setInterpolator(new BounceInterpolator());
-		mapScaleAnim.start();
-		mapZoomHandler.handleMessage(null);
-	}
-	
-	
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+            float distanceY) {
+
+        if (Boolean.FALSE.equals(scaling)) {
+            scaling = null;
+            return false;
+        }
+        if (scaling != null)
+            return false;
+        return super.onScroll(e1, e2, distanceX, distanceY);
+    }
+
+    @Override
+    public boolean onScale(android.view.ScaleGestureDetector detector) {
+        if (!zooming()) {
+            savedMatrix.set(matrix);
+            matrix.postScale(detector.getScaleFactor(), detector.getScaleFactor(), detector.getFocusX(), detector.getFocusY());
+            matrix.getValues(matrixValues);
+            if (matrixValues[Matrix.MSCALE_X] < matrixOriginValues[Matrix.MSCALE_X] / 1.5f ||
+                    matrixValues[Matrix.MSCALE_X] > matrixOriginValues[Matrix.MSCALE_X] * 5f) {
+                matrix.set(savedMatrix);
+            } else {
+                invalidate();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(android.view.ScaleGestureDetector detector) {
+        mapListener.onSingleTapCancelled();
+        mapListener.onTouchScale(detector.getScaleFactor(),
+                detector.getFocusX(), detector.getFocusY());
+        scaling = true;
+        zoomed = true;
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(android.view.ScaleGestureDetector detector) {
+        Log.i(getClass().getSimpleName(), "Scale End");
+
+        matrix.getValues(matrixValues);
+        scaling = false;
+
+        if (matrixValues[Matrix.MSCALE_X] < matrixOriginValues[Matrix.MSCALE_X]) {
+            mapScaleAnim = new MapScaleAnim(matrix, matrixOrigin, 200);
+            zoomed = false;
+        } else if (matrixValues[Matrix.MSCALE_X] > matrixOriginValues[Matrix.MSCALE_X] * 3f) {
+            mapScaleAnim = new MapScaleAnim(matrix, getWidth() / 2, getHeight() / 2, getWidth() / 2, getHeight() / 2,
+                    matrixOriginValues[Matrix.MSCALE_X] / matrixValues[Matrix.MSCALE_X] * 3f, 200);
+        } else {
+            return;
+        }
+
+        mapScaleAnim.initialize(0, 0, getWidth(), getHeight());
+        mapScaleAnim.setInterpolator(new BounceInterpolator());
+        mapScaleAnim.start();
+        mapZoomHandler.handleMessage(null);
+    }
+
 }
